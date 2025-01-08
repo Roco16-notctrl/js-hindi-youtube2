c58a9431982a30371c4fae0a11f6e5535bf7bd86
Roco16-notctrl <aaditya5656@icloud.com>
2025-01-08 02:08:20 +0000

    datatypes learned

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"Use strict";//treat all js code as newer version
+
+// alert("hello");  //we are using node js  not browser
+
+console.log(3+3); console.log("hitesh")//code readability ki maa chud gyi vaiya g.
+
+// data-types
+// number =>2 to power 53
+//bigint
+//string=>""
+// undefined
+//symbol =>unique
+
+console.log(typeof undefined)
