4db18e3b3e9051837781be86aefe34bcb153d747
Roco16-notctrl <aaditya5656@icloud.com>
2025-01-08 02:55:45 +0000

    push

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,95 @@
+
+// let score="33"
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+//  valueInNumber=score
+// console.log(typeof valueInNumber)
+
+
+// let score ="33abd"
+// console.log(typeof score)
+// let valueInNumber=Number(score)
+// console.log(valueInNumber)
+// console.log(typeof(valueInNumber))
+
+
+// let score = null
+// console.log(typeof(score))
+// let valueInNumber=Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+
+
+
+// let score = undefined
+// console.log(typeof(score))
+// let valueInNumber=Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+
+
+// let score = true
+// console.log(typeof(score))
+// let valueInNumber=Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+
+
+// let score = false
+// console.log(typeof(score))
+// let valueInNumber=Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+
+
+
+//"33"=>33
+//"33abc"=>NaN
+//true =>1; false=>0;
+
+// let isLoggedIn=1
+// let booleanIsloggedIN=Boolean(isLoggedIn)
+// console.log(booleanIsloggedIN)
+
+
+// let isLoggedIn=0
+// let booleanIsloggedIN=Boolean(isLoggedIn)
+// console.log(booleanIsloggedIN)
+
+// let isLoggedIn=""
+// let booleanIsloggedIN=Boolean(isLoggedIn)
+// console.log(booleanIsloggedIN)
+
+
+// let isLoggedIn="aarya"
+// let booleanIsloggedIN=Boolean(isLoggedIn)
+// console.log(booleanIsloggedIN)
+
+
+// let isLoggedIn=3
+// let booleanIsloggedIN=Boolean(isLoggedIn)
+// console.log(booleanIsloggedIN)
+
+
+
+
+//1=>true ; 0=>false
+//""=>false
+//"hitesh"=>true
+
+
+
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
