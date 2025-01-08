f4919cf9c14a403f081fb7b4814c445a36cf5fc0
Roco16-notctrl <aaditya5656@icloud.com>
2025-01-08 03:27:33 +0000

    operations in javascript learned

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -93,3 +93,51 @@
 // let stringNumber=String(someNumber)
 // console.log(stringNumber)
 // console.log(typeof stringNumber)
+
+
+
+//***************************************Operations*******************************/
+
+// let value=3
+// let negValue=-value
+// console.log(value)
+// console.log(negValue)
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+// let str1="hello"
+// let str2=" sumi"
+
+// let str3=str1+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log(true)
+// console.log(+true)
+// console.log(+"")
+
+
+// let num1,num2,num3
+// num1=num2=num3=2+2
+// console.log(num1,num2,num3)
+
+let gamecounter=100
+// gamecounter++
+++gamecounter
+console.log(gamecounter)
+
+
+//x++ this increment operators increments and return the value before incrementing
+//++x this increment operators increments and return the value after incrementing
+//You cannot chain multiple increments together  ++(++x).This is wrong.
+
+// let x2=3n;
+// let y2=x2++;
+// console.log(x2 , y2)
