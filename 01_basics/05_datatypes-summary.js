6e81ad6d28ab5b8be347a96dea67c687250125e4
Roco16-notctrl <aaditya5656@icloud.com>
2025-01-08 04:58:52 +0000

    summary learned

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,44 @@
+// Primitive(7 types)
+/*
+   (string: gives copy of string data type not a original value,Number),Boolean,null,undefined,symbol,BigInt
+*/
+// const score=100
+// const scoreValue=100.3
+
+// const isLoggedIn=false
+const outsideTemp=null
+// let userEmail=undefined
+
+const anotherid=Symbol('2133423')
+// const id=Symbol('2133423')
+// console.log(anotherid)
+// console.log(id)
+// console.log(id===anotherid)
+
+const bigNumber=3982373784747n
+// console.log(bigNumber)
+
+
+//Reference (Non primitive)
+//Array,Objects,Functions
+
+
+const heros=["shaktiman","sunnyleone","doga"]
+// console.log(heros)
+let myObj={
+    name:"hitesh",
+    age:22,
+}
+console.log(myObj.name)
+console.log(myObj.age)
+
+
+const myFunction=function(){
+    console.log("hello world")
+}
+
+console.log(typeof(bigNumber))
+console.log(typeof(outsideTemp))
+console.log(typeof(myObj))
+console.log(typeof(myFunction))
+console.log(typeof(anotherid))
